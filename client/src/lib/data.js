0dfb2ef985788544be6f20a31e38e5875b5e2313
ssxv <satyendra.singh@talentica.com>
2024-01-27 22:59:32 +0530

    change app name

--- client/src/lib/data.js
+++ client/src/lib/data.js
@@ -1,3 +1,4 @@
+export const APP_NAME = 'Conversio';
 export const API_SERVER_URL = 'http://localhost:9000/api';
 export const SOCKET_SERVER_URL = 'http://localhost:9000/chat';
 export const SOCKET_SERVER_EVENT = {
@@ -8,17 +9,3 @@ export const SOCKET_SERVER_EVENT = {
     CLIENT_TYPING: 'client-typing',
     MESSAGE_READ: 'message-read',
 };
-
-/**
- * {
- *   userId1: [
- *      { from: userId1, to: userId0 },
- *      { from: userId0, to: userId1 },   
- *   ],
- *   userId2: [
- *      { from: userId2, to: userId0 },
- *      { from: userId0, to: userId2 },   
- *   ],
- * }
- */
-export const MESSAGE_STORE = {};
